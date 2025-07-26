cd3c4c716e1d100784d3792065803df496dc9fc4
Leon Yu <5331469+clyu@users.noreply.github.com>
2025-07-26 22:02:59 +0800

    beautify http header insertion

--- src/main.rs
+++ src/main.rs
@@ -61,16 +61,20 @@ struct Comic {
 impl Comic {
     fn new(id: usize, tunnel: usize, delay_ms: u64, skip: bool, output_dir: String) -> io::Result<Self> {
         let mut headers = HeaderMap::new();
-        headers.insert("accept", "image/webp,image/apng,image/*,*/*;q=0.8".parse().unwrap());
-        headers.insert("accept-encoding", "gzip, deflate, br".parse().unwrap());
-        headers.insert("accept-language", "zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7,zh-CN;q=0.6".parse().unwrap());
-        headers.insert("cache-control", "no-cache".parse().unwrap());
-        headers.insert("pragma", "no-cache".parse().unwrap());
-        headers.insert("referer", "https://www.manhuagui.com/".parse().unwrap());
-        headers.insert("sec-fetch-dest", "image".parse().unwrap());
-        headers.insert("sec-fetch-mode", "no-cors".parse().unwrap());
-        headers.insert("sec-fetch-site", "cross-site".parse().unwrap());
-        headers.insert("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36".parse().unwrap());
+        for (key, value) in &[
+            ("accept", "image/webp,image/apng,image/*,*/*;q=0.8"),
+            ("accept-encoding", "gzip, deflate, br"),
+            ("accept-language", "zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7,zh-CN;q=0.6"),
+            ("cache-control", "no-cache"),
+            ("pragma", "no-cache"),
+            ("referer", "https://www.manhuagui.com/"),
+            ("sec-fetch-dest", "image"),
+            ("sec-fetch-mode", "no-cors"),
+            ("sec-fetch-site", "cross-site"),
+            ("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36"),
+        ] {
+            headers.insert(*key, value.parse().unwrap());
+        }
         let client = Client::builder().default_headers(headers).build().unwrap();
         let host = String::from("https://tw.manhuagui.com");
         let channels = ["i", "eu", "us"];
